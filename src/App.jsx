bb01c6dbd9751532e6e50dfd4695c9ba205a87c3
ian apud <ianapud23@gmail.com>
2025-03-27 21:10:34 -0300

    inicio del repositorio para el portafolio comprobacion de tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+
+  return (
+    <>
+      <div className='bg-[#222222] dark:bg-[#121212] w-screen h-full min-h-[1080px]  overflow-x-hidden p-2'>
+      </div>
+    </>
+  )
+}
+
+export default App
